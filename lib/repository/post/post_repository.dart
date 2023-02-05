bb7e20f740c4a7e7bdefcfebf0d897e6b71c56c1
tsurumoto <tsurumotokentarou@gmail.com>
2023-02-05 10:34:41 +0900

    change directory

--- lib/repository/post/post_repository.dart
+++ lib/repository/post/post_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_sample_retrofit/api_client/api_client.dart';
+import 'package:flutter_sample_retrofit/model/post/post_list_response.dart';
 import 'package:flutter_sample_retrofit/model/result.dart';
-import 'package:flutter_sample_retrofit/page/post_list/model/post_list_response.dart';
 
 abstract class PostRepository {
   const PostRepository();
